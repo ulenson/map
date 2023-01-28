e23e2ed7d40a6c2a6bab4de35a4282e34d8bff8b
ulenson <114535397+ulenson@users.noreply.github.com>
2023-01-28 23:51:37 +0300

    Update saved_locations.dart

--- lib/model/saved_locations.dart
+++ lib/model/saved_locations.dart
@@ -1,16 +1,5 @@
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
-// class SavedLocation {
-//   // Map<String, dynamic> map;
-//    String name;
-//    Point point;
-//
-//   SavedLocation({ required
-//       this.name,
-//       required this.point,}
-//       );
-// }
-import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 class SavedLocation {
   final String name;
